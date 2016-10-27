7574424dce6358ea1dcd20404c522d55e93202c8
shshunda <shshunda@ZONE04-ED.cs.ucalgary.ca>
2016-10-27 10:58:07 -0600

    Initializing Assignment 2, with test driver files added.

---
+++ src/cpsc501_a2/pkg0/CPSC501_A20.java
@@ -0,0 +1,16 @@
+package cpsc501_a2.pkg0;
+
+/**
+ *
+ * @author shshunda
+ */
+public class CPSC501_A20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

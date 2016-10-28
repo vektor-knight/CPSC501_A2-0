977b01d4f94563ac762e821c2004b3fedabefe53
shshunda <shshunda@ZONE04-ED.cs.ucalgary.ca>
2016-10-27 19:37:12 -0600

    Three library methods implemented before refactoring and testing.

--- src/cpsc501_a2/pkg0/Inspector.java
+++ src/cpsc501_a2/pkg0/Inspector.java
@@ -24,21 +24,53 @@ public class Inspector {
         Class classObject = obj.getClass();
         
         /*
-        * Programming Plan, before applying any refactoring
-        * or unit testing. Most of these are just library method calls.
-        * Referencing the Java library: (java.lang.reflect)
+         * Programming Plan, before applying any refactoring
+         * or unit testing. Most of these are just library method calls.
+         * Referencing the Java library: (java.lang.reflect)
+         */
+        // Name of the declaring class
+        Class declaringClass = classObject.getDeclaringClass();
+        System.out.println("INSPECTING THE DECLARING CLASS");
+        System.out.println("The declaring class is: " + declaringClass);
+        System.out.println("DONE INSPECTING THE DECLARING CLASS");
+        
+        // Name of the declaring superclass
+        Class declaringSuper = classObject.getSuperclass();
+        System.out.println("INSPECTING THE DECLARING CLASS");
+        System.out.println("The declaring class is: " + declaringSuper);
+        System.out.println("DONE INSPECTING THE DECLARING CLASS");
+        
+        /*
+        * It is worth noting that at this stage, all of these
+        * above snippets of code would refactor into void methods
+        * with side-effects. I will have to declare a returning
+        * type and then make a query to that method, and THEN
+        * print it out. However, this is just a sequence of what
+        * my program ought to be doing at this stage.
         */
-        // Name of the declaring class 
-        // Name of the declaring superclass 
+        
         // Name of the interfaces the class implements
+        Class[] classInterfaces = classObject.getInterfaces();
+        // Since the interfaces are returned in an array,
+        // traverse it.
+        // In this case, we are only asked for the name, although
+        // it is possible to retrieve other data about the interfaec
+        // after each iteration.
+        if (classInterfaces.length > 0) {
+            System.out.println("INSPECTING THE CLASS INTERFACES");
+            for (Class classInterface : classInterfaces) {
+                System.out.println("Interface: " + classInterface);
+            }
+        }
+        
         // Methods the class declares
-            // Exceptions
-            // Parameter types
-            // Return type
-            // Modifiers
+        // Exceptions
+        // Parameter types
+        // Return type
+        // Modifiers
         // Constructors the class declares
-            // Type
-            // Modifiers
+        // Type
+        // Modifiers
         // Current value of each field. If recursive == false, print
         // out the reference value.
         
